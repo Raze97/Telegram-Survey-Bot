9264277bd6d437c0abac4e256520205b6a4cd311
Marcel <marcel.killinger@gmx.de>
2020-02-07 09:55:55 +0100

    Add license commands

--- bot_utils/bot_util_types.py
+++ bot_utils/bot_util_types.py
@@ -1,3 +1,22 @@
+"""
+Copyright: (c) 2020, Michael Barthelmäs, Marcel Killinger, Johannes Keller
+GNU General Public License v3.0 (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+This file is part of Telegram Survey Bot.
+
+Telegram Survey Bot is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Telegram Survey Bot is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Telegram Survey Bot.  If not, see <http://www.gnu.org/licenses/>.
+"""
 from datetime import time
 from typing import List
 
